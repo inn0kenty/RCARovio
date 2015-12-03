1c3a10df606945af55ee19028d2e72e36e7802d0
inn0kenty <innlebedev@gmail.com>
2015-12-03 23:28:15 +0300

    Improved test structure

--- main/main.py
+++ main/main.py
@@ -1,14 +1,15 @@
 from roviotest import tests
 from . import parser
+import sys
 
 def main():
     args = parser.parse_args()
-
-    test = tests.Test()
-
-    if args.ping:
-        test.ping()
-    if args.test:
-        test.syntax()
-
     file_path = args.file_path[0]
+
+    if len(sys.argv) > 2:
+        test = tests.Test(file_path)
+        if args.ping:
+            test.ping()
+        if args.test:
+            test.syntax()
+
--- roviotest/tests.py
+++ roviotest/tests.py
@@ -1,4 +1,7 @@
 class Test(object):
+    def __init__(self, file_path):
+        self.__file_path = file_path
+
     def syntax(self):
         print 'test'
 
