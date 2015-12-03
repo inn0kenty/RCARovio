029d02d67258fe0055b7eccd4e19c51b297313d8
inn0kenty <innlebedev@gmail.com>
2015-12-03 23:37:30 +0300

    Added check for file existence

--- main/main.py
+++ main/main.py
@@ -1,10 +1,15 @@
 from roviotest import tests
 from . import parser
 import sys
+import os.path
 
 def main():
     args = parser.parse_args()
     file_path = args.file_path[0]
+
+    if not os.path.isfile(file_path):
+        print 'File not found'
+        exit(-1);
 
     if len(sys.argv) > 2:
         test = tests.Test(file_path)
