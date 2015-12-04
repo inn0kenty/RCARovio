e41c919f73855ca74b5465034827d98711991ec5
inn0kenty <innlebedev@gmail.com>
2015-12-04 23:33:08 +0300

    Added the use of parser

--- main/main.py
+++ main/main.py
@@ -2,6 +2,7 @@ from test import tests
 from . import parser
 import sys
 import os.path
+from utils import utils
 
 def main():
     args = parser.parse_args()
@@ -11,8 +12,11 @@ def main():
         print 'File not found'
         exit(-1);
 
+    file_parser = utils.FileParser(file_path)
+    commands = file_parser.get_commands()
+
     if len(sys.argv) > 2:
-        test = tests.Test(file_path)
+        test = tests.Test(commands)
         if args.ping:
             test.ping()
         if args.test:
