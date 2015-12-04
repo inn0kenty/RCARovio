05cda687ab6d34a4e47ad993053ec4592571a866
inn0kenty <innlebedev@gmail.com>
2015-12-04 23:30:25 +0300

    Add file parser

--- utils/utils.py
+++ utils/utils.py
@@ -25,20 +25,32 @@ class Commands(object):
         else:
             return None
 
-    def __ite r__(self):
-        self.__position = 0
+    def __iter__(self):
+        self.__position = -1
         return self
 
     def next(self):
-        if self.__position < len(self.__commands):
+        if self.__position < len(self.__commands) - 1:
+            self.__position += 1
             return self.__commands[self.__position]
         else:
             raise StopIteration()
 
 
+class FileParser(object):
+    def __init__(self, file_path):
+        self.__file_path = file_path
+        self.__commands = Commands()
 
-#class FileParser(object):
-#    def __init__(self, file_path):
-#        self.__file_path = file_path
+        command_file = open(file_path, 'r')
+        for command in command_file:
+            if command != '\n':
+                self.__commands.add_command(self.__parse_command(command))
 
+    def get_commands(self):
+        return self.__commands
 
+    def __parse_command(self, command):
+        command = command.strip('\n')
+        command = (' '.join(command.split())).split(' ')
+        return command
