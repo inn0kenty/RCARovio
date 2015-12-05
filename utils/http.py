9c7394ce221e8752c61fcb21745cfe59f369eb00
inn0kenty <innlebedev@gmail.com>
2015-12-05 15:20:45 +0300

    Some implementation of http

--- utils/http.py
+++ utils/http.py
@@ -5,12 +5,18 @@ import urllib
 class HTTPRequests (object):
     def __init__(self, ip):
         self.__ip = ip
+        self.__image_handler = None
 
-    def move(self):
+    def add_image_handler(self, image_handler):
+        self.__image_handler = image_handler
+
+    def move(self, speed, time):
         pass
 
     def cap_image(self):
-        pass
+        file_name = ""
+        if self.__image_handler:
+            self.__image_handler(file_name)
 
     def ping(self):
         connection = httplib.HTTPConnection(self.__ip, timeout=10)
@@ -28,12 +34,12 @@ class HTTPRequests (object):
         response = response.read().strip('\n')
         connection.close()
 
-        #response parameter
+        # response parameter
         ping = response.split('\n')[1].split('|')[0].split('=')
         # battery parameter
         battery = response.split('\n')[7].split('|')[1].split('=')
 
-        if int(ping[1]) == 0: # if response parameter 0 then all ok
+        if int(ping[1]) == 0:  # if response parameter 0 then all ok
             return True, battery[1]
         else:
             return False, None
